e8e275f1086db6991a22a373b1126b774be03654
Darkzos <112402820+Darkzos@users.noreply.github.com>
2022-10-19 14:37:18 -0500

    Create codigo.py

---
+++ codigo.py
@@ -0,0 +1,14 @@
+def temaCuatro(numeroif):
+   numeroif= input()
+   if numeroif < 0:
+      print("el numero es negativo")
+   elif numeroif == 0:
+      print("el numero es 0")
+   elif numeroif > 0:
+      print("el numero es positivo")
+   int(numerowhile = 0)
+   while numerowhile<3:
+      print(numerowhile)
+      numerowhile++
+   numeroFor = 0
+   for numeroFor 
